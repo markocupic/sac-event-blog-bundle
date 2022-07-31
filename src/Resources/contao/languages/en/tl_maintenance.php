59eba5fcafa52c0771a60ba0e6150ceae1384689
Marko Cupic <m.cupic@gmx.ch>
2022-07-31 11:14:20 +0200

    Add language file for the backend maintainance task

---
+++ src/Resources/contao/languages/en/tl_maintenance.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SAC Event Blog Bundle.
+ *
+ * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/sac-event-blog-bundle
+ */
+
+$GLOBALS['TL_LANG']['tl_maintenance_jobs']['sac_event_blog']['0'] = 'Tourenberichte Module bereinigen';
+$GLOBALS['TL_LANG']['tl_maintenance_jobs']['sac_event_blog']['1'] = 'Löscht Bildupload-Verzeichnisse, die keinem Bericht zugeordnet werden können.';
